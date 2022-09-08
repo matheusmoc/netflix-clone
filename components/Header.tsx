3796a1366a1f2af8e793e58d544f12c4520e78cf
Matheus Andrade <matheusandrade009@gmail.com>
2022-09-08 00:46:54 -0300

    hero icons and navbar

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import {BellIcon, MagnifyingGlassIcon} from "@heroicons/react/24/solid"
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+            <li className="headerLink">Home</li>
+            <li className="headerLink">TV Show</li>
+            <li className="headerLink">Movies</li>
+            <li className="headerLink">New & Popular</li>
+            <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div>
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline"/>
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
