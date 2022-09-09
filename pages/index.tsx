35364cf126a84bcee3ca919b591b2ba1f8cef5a8
Matheus Andrade <matheusandrade009@gmail.com>
2022-09-09 19:08:56 -0300

    Create row component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Row from "../components/Row";
 import { Movie } from "../typing";
 import requests from "../utils/requests";
 
@@ -36,7 +37,16 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-8 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals}/>
         <section>
-
+          
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        
         </section>
       </main>
     </div>
